c937d0301a8af0b0b305ef9852bb26e92132a542
Ido Tausi <ido.tausi@gmail.com>
2023-01-06 12:45:26 +0200

    first commit

---
+++ mult16x16.s
@@ -0,0 +1,59 @@
+# Operands to multiply
+.data
+a: .word 0xBAD
+b: .word 0xFEED
+
+.text
+main:   # Load data from memory
+	la      t3, a
+        lw      t3, 0(t3)
+        la      t4, b
+        lw      t4, 0(t4)
+        
+        # t6 will contain the result
+        add	t6, x0, x0
+
+        # Mask for 16x8=24 multiply
+        ori	t0, x0, 0xff
+        slli	t0, t0, 8
+        ori	t0, t0, 0xff
+        slli	t0, t0, 8
+        ori	t0, t0, 0xff
+        
+####################
+# Start of your code
+
+# Use the code below for 16x8 multiplication
+#   mul		<PROD>, <FACTOR1>, <FACTOR2>
+#   and		<PROD>, <PROD>, t0
+
+
+        andi t1, t3, 0xff
+
+        # first 8 x 16
+        mul t2, t1, t4
+        and t2, t2, t0
+        # no shift needed
+        add t6, t6, t2
+
+        # second 8 x 16
+        srli t3, t3, 8
+        andi t1, t3, 0xff
+
+        mul t2, t1, t4
+        and t2, t2, t0
+        # shift needed
+        slli t2, t2, 8
+        add t6, t6, t2
+        
+        
+# End of your code
+####################
+		
+finish: addi    a0, x0, 1
+        addi    a1, t6, 0
+        ecall # print integer ecall
+        addi    a0, x0, 10
+        ecall # terminate ecall
+
+
